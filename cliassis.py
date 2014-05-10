15d856efa5d4030d6ae92b4e7df4ccff6a97f028
valeness <valeness84@gmail.com>
2014-05-09 22:35:56 -0500

    Update cliassis.py

--- cliassis.py
+++ cliassis.py
@@ -3,76 +3,27 @@ import urllib
 import json as m_json
 import os
 import webbrowser
-#from twisted.internet import reactor
-#from twisted.internet import protocol
+import random
+import ConfigParser
+#actual sending function
+import smtplib
+#import the email modules
+from email.mime.text import MIMEText
 
-#commented out for later implementation
+config = ConfigParser.RawConfigParser()
+config.read('config.cfg')
 
-##class MyProcessProtocol(protocol.ProcessProtocol):
-#	def __init_(self, text):
-#		self.text = text
-		
-#	def connectionMade(self):
-#		self.transport.write(self.text)
-#		self.transport.closeStdin()
-		
-	
-
-#define the start function to set directry and commands
-def start():
-	
-	#start setting variables for command precursor
-	print """
-	-----------------------------------------------------------------
-	Here we will set some variables that will allow you to 
-	customize your commands. All commands follow this procedure 
-	"[command] [string/query]". End all directories with a backslash or
-	else it will not work. Please refer to the readme for more.
-	-----------------------------------------------------------------
-	"""
-	
-	print "-----------------------------------------"
-	print "First, set your program shortcuts directory. Remember the Backslash"
-	print "-----------------------------------------"
-	directory = raw_input("Directory:")
-	
-	print "-----------------------------------------"
-	print "Google Command"
-	print "-----------------------------------------"
-	google = raw_input("Command:")
-	
-	print "-----------------------------------------"
-	print "Command to Search Browser in Google"
-	print "-----------------------------------------"
-	searchbrowser = raw_input("Command:")
-	
-	print "-----------------------------------------"
-	print "Command to Search in Duck. In Browser."
-	print "-----------------------------------------"
-	duckbrowser = raw_input("Command:")
-	
-	print "-----------------------------------------"
-	print "Command to Open a file in your choosen directory."
-	print "-----------------------------------------"
-	openf = raw_input("Command:")
-	
-	print "-----------------------------------------"
-	print "Command to List Available Prgrams"
-	print "-----------------------------------------"
-	lis = raw_input("Command:")
-	
-	
-	#write custom commands to data.txt file | each on a new line
-	f = open('data.txt', 'w')
-	f.write(directory+"\n") #Line 0
-	f.write(google+"\n") #Line 1
-	f.write(searchbrowser+"\n") #Line 2
-	f.write(duckbrowser+"\n") #Line 3
-	f.write(openf+"\n") #Line 4
-	f.write(lis+"\n") #Line 5
-	f.close()
-
-#processProtocol = MyProcessProtocol()
+#get variables from config.cfg
+directory = config.get('directories', 'shortcut_directory')
+google = config.get('commands', 'google_command')
+searchbrowser = config.get('commands', 'google_browser')
+duckbrowser = config.get('commands', 'duck_browser')
+openf = config.get('commands', 'open_file')
+lis = config.get('commands', 'list_shortcuts')
+blog = config.get('commands', 'blog_command')
+sender = config.get('email', 'your_email')
+receiver = config.get('email', 'blog_email')
+weburl = config.get('email', 'blog_mail_url')
 
 #define main function
 def command():
@@ -83,7 +34,7 @@ def command():
 	#start checking for command precursors and decide what to do
 	#when you find it
 	
-	if google.rstrip('\n') in command:
+	if google in command:
 		query = command.replace(google, "") #remove the precursor command
 		query = urllib.urlencode ({ 'q' : query }) #encode the query
 		response = urllib.urlopen('http://ajax.googleapis.com/ajax/services/search/web?v=1.0&' + query).read() #send the query to google API
@@ -125,7 +76,16 @@ def command():
 		
 	if lis in command:
 		newdir = directory.replace("\\", "", 1) #clean up directory
-		os.system('dir ' + newdir) #list contents of shortcut directory
+		os.system('dir ' + directory) #list contents of shortcut directory
+	
+	if joke in command:
+		random.choice(jokes)()
+		
+	if 'tries' in command:
+		print 'Loops: %s' % tries
+		
+	if blog in command:
+		post()
 	
 	#create array for possible quit commands
 	shut = {'shut', 'quit', 'stop', 'kill', 'exit', 'close'}
@@ -148,10 +108,6 @@ def command():
 #check to see if data.txt contains any data
 #if it doesn't, call start() to assign variables
 #if it does, continue
-if len(open('data.txt').readlines()) < 1:
-	start()
-else:
-	pass
 
 #read variables from data.txt to be useable outside the start function
 #and then reassign them
@@ -163,6 +119,12 @@ searchbrowser = lines[2].rstrip('\n')
 duckbrowser = lines[3].rstrip('\n')
 openf = lines[4].rstrip('\n')
 lis = lines[5].rstrip('\n')
+joke = lines[6].rstrip('\n')
+blog = lines[7].rstrip('\n')
+sender = lines[8].rstrip('\n')
+receiver = lines[9].rstrip('\n')
+weburl = lines[10].rstrip('\n')
+
 f.close()
 
 #show the user what his/her custom commands are
@@ -173,12 +135,70 @@ print "[%s]" % searchbrowser.rstrip('\n')
 print "[%s]" % openf.rstrip('\n')
 print "[%s]" % duckbrowser.rstrip('\n')
 print "[%s]" % lis.rstrip('\n')
+print "[%s]" % blog.rstrip('\n')
 print "[Wipe Cache]"
 print "quit/kill/exit/shut"
 print "-----------------------------------------"
 
+#set joke functions
+def joke1():
+	print 'Why did the Rabbit cross the road?'
+	loop = 0
+	while loop == 0:
+		ans = raw_input('reply:').lower()
+		if 'why' in ans:
+			print 'To eat the chicken!'
+			loop = 1
+			break
+		print 'Go on! Ask why!'	
+
+def joke2():
+	print 'Glenn'
+
+#create array for jokes
+jokes = [joke1, joke2]
+
+tries = 0
+
+def post():
+	post = open('post.txt', 'w')
+	title = raw_input('title>>')
+	while 1:
+		message = raw_input('>>')
+		post.write(message + '\n')
+		
+		if '[done]' in message:
+			post = open('post.txt', 'r')
+			msg = MIMEText(post.read())
+			post.close
+			
+			msg['Subject'] = title
+			msg['From'] = sender
+			msg['To'] = receiver
+			s = smtplib.SMTP('smtp.gmail.com:587')
+			s.starttls()
+			user = raw_input('Username/Email>>')
+			password = raw_input('Password>>')
+			s.login(user, password)
+			s.sendmail(sender, [receiver], msg.as_string())
+			s.quit()
+			print 'Do you want to post this message?'
+			print 'This requires your browser. Only accepts yes/no'
+			print '---------------------------------'
+			send = raw_input('choice>>')
+			
+			new = 2
+			
+			if 'yes' in send:
+				webbrowser.open(weburl, new=new)
+				break
+			elif 'no' in send:
+				break
+		
+			
 #loop forever
 #will ask for another command after one executes; infinitely
 #will break if error occurs
 while 1:
 	command()
+	tries = tries + 1
