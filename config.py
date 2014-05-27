a03a950aa82f459f2474d72c27177f3c62d966a3
valeness <valeness84@gmail.com>
2014-05-26 22:03:45 -0500

    added new commands and sections

--- config.py
+++ config.py
@@ -5,6 +5,8 @@ config = ConfigParser.RawConfigParser()
 config.add_section('directories')
 config.add_section('commands')
 config.add_section('email')
+config.add_section('language')
+config.add_section('nicknames')
 
 config.set('directories', 'shortcut_directory', 'C:\shortcuts')
 config.set('commands', 'google_command', 'google')
@@ -17,6 +19,14 @@ config.set('email', 'your_email', 'example@example.com')
 config.set('email', 'blog_email', 'blog@example.com')
 config.set('email', 'blog_mail_url', 'yourdomain.com/blog/wp-content.php')
 config.set('commands', 'journal', 'write')
+config.set('commands', 'search_journal', 'search')
+config.set('commands', 'language_processing', 'key')
+config.set('commands', 'add_nickname', 'add')
+
+config.set('language', 'keyword_context', 'search')
+
+config.set('nicknames', 'python', 'http://python.org')
+config.set('nicknames', 'vales', 'http://valeness.tk')
 
 with open('config.cfg', 'wb') as configfile:
 	config.write(configfile)
